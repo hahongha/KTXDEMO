ec281367a10a122aea47ed26f9c8026b2264d6e6
tuantoutc <142429308+tuantoutc@users.noreply.github.com>
2024-12-01 15:52:58 +0700

    Update TransactionDTO.java

--- src/main/java/com/utc/dormitory_managing/dto/TransactionDTO.java
+++ src/main/java/com/utc/dormitory_managing/dto/TransactionDTO.java
@@ -2,11 +2,15 @@ package com.utc.dormitory_managing.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class TransactionDTO {
     private String name;
     private String description;
-    private String data;
+    private String orderInfo;
+    private Date date;
+    private Long amount;
 }
 
