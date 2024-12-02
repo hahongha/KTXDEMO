704097073092dfb69cec92e3a1f02220857e63e6
tuantoutc <142429308+tuantoutc@users.noreply.github.com>
2024-12-02 16:41:33 +0700

    Update PaymentRepo.java

--- src/main/java/com/utc/dormitory_managing/repository/PaymentRepo.java
+++ src/main/java/com/utc/dormitory_managing/repository/PaymentRepo.java
@@ -4,4 +4,6 @@ import com.utc.dormitory_managing.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepo extends JpaRepository<Payment, Long> {
+  @Query("SELECT p FROM Payment p WHERE p.paymentInfo LIKE %:searchTerm%")
+    List<Payment> searchByTerm(@Param("searchTerm") String searchTerm);
 }
