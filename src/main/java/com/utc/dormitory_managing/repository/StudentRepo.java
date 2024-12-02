84aeab2dfecd3d32cfb7201fa601f97cde05d207
tuantoutc <142429308+tuantoutc@users.noreply.github.com>
2024-12-02 21:20:01 +0700

    Update StudentRepo.java

--- src/main/java/com/utc/dormitory_managing/repository/StudentRepo.java
+++ src/main/java/com/utc/dormitory_managing/repository/StudentRepo.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import com.utc.dormitory_managing.entity.Student;
 
 public interface StudentRepo extends JpaRepository<Student, String> {
+	@Query("SELECT s FROM Student s WHERE s.studentEmail = :x")
 	Optional<Student> findByStudentEmail(@Param("x") String email);
 	
 	@Query("SELECT Count(a.studentId) from Student a where a.room.roomId = :x and a.studentStatus = 0")
